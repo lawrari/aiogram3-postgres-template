eaf9c8a87a40935af1337d7a1872f05eb8f26759
iwseen <haterari1@gmail.com>
2024-03-08 11:18:08 +0300

    third

--- bot/filters/admin.py
+++ bot/filters/admin.py
@@ -6,6 +6,7 @@ from config.config import Config
 
 class AdminFilter(BaseFilter):
     is_admin: bool = True
+    # some useful comment
 
     async def __call__(self, obj: Message, config: Config) -> bool:
         return (obj.from_user.id in config.telegram_bot.admin_ids) == self.is_admin
